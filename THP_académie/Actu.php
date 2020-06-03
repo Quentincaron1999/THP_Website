e47bc83d0aabd8308480ccda71ec966a23cd2cfc
Quentincaron1999 <quentincaron1999@live.fr>
2020-06-03 11:37:26 +0200

    Update

--- THP_académie/Actu.php
+++ THP_académie/Actu.php
@@ -16,7 +16,7 @@
                     <h1>Article</h1>
                 </div>
                 <div class="col-md-4">
-                    <h1>Facebook</h1>
+                    <h1 style="margin-bottom: 8%;">Facebook</h1>
                     <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fthpacademie%2Fposts%2F1050757068653588&show_text=true&width=552&height=515&appId" width="80%" height="" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
                         <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fthpacademie%2Fposts%2F1002882230107739&show_text=true&width=552&height=534&appId" width="80%" height="" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
                 </div>
