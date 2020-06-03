86aab7975db548844cb4b1bbda85390056568b3f
Quentincaron1999 <quentincaron1999@live.fr>
2020-06-03 10:27:31 +0200

    Mojor modif - actu

--- THP_académie/Actu.php
+++ THP_académie/Actu.php
@@ -17,11 +17,53 @@
                 </div>
                 <div class="col-md-4">
                     <h1>Facebook</h1>
-                    <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fthpacademie%2Fposts%2F1050757068653588&show_text=true&width=552&height=515&appId" width="552" height="515" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+                    <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fthpacademie%2Fposts%2F1050757068653588&show_text=true&width=552&height=515&appId" width="80%" height="" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+                        <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fthpacademie%2Fposts%2F1002882230107739&show_text=true&width=552&height=534&appId" width="80%" height="" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
                 </div>
                 <div class="col-md-4">
                     <h1>Témoignages</h1>
                     
+                    <div class="row people" style="margin-top: 3em;">
+                <div class="actutem">
+                    <div class="author"><iframe allowfullscreen="" frameborder="0" src="https://www.youtube-nocookie.com/embed/RiGs0YiPDtg?autoplay=1&amp;mute=1&amp;loop=1&amp;playlist=RiGs0YiPDtg&amp;controls=0" style="height: 134px;width: 235px;margin-top: -21px;"></iframe>
+                        <h5
+                            class="name" style="color: #f7eed1;">Vincent G</h5>
+                            <p class="title" style="color: #f7eed1;margin-left: 2px;">Participant 2019</p>
+                    </div>
+                </div>
+                <div class="actutem" style="margin-top: 2em;">
+                    <div class="author"><iframe allowfullscreen="" frameborder="0" src="https://www.youtube-nocookie.com/embed/pSN_53lo-uo?autoplay=1&amp;mute=1&amp;loop=1&amp;playlist=pSN_53lo-uo&amp;controls=0" style="height: 134px;width: 235px;margin-top: -21px;"></iframe>
+                        <h5
+                            class="name" style="font-weight: bold;color: #f7eed1;">Cathy A</h5>
+                            <p class="title" style="color: #f7eed1;margin-left: 1px;">Participante 2019</p>
+                    </div>
+                </div>
+                <div class="actutem" style="margin-top: 2em;">
+                    <div class="author"><iframe allowfullscreen="" frameborder="0" src="https://www.youtube-nocookie.com/embed/5S0_MXcVGPg?autoplay=1&amp;mute=1&amp;loop=1&amp;playlist=5S0_MXcVGPg&amp;controls=0" style="height: 134px;width: 235px;margin-top: -21px;"></iframe>
+                        <h5
+                            class="name" style="font-weight: bold;color: #f7eed1;">Jérôme D</h5>
+                            <p class="title" style="color: #f7eed1;margin-left: 1px;">Participant 2019</p>
+                    </div>
+                </div>
+                <div class="actutem" style="margin-top: 2em;">
+                    <div class="author"><iframe allowfullscreen="" frameborder="0" src="https://www.youtube-nocookie.com/embed/kT6I_R0J_GU?autoplay=1&amp;mute=1&amp;loop=1&amp;playlist=kT6I_R0J_GU&amp;controls=0" style="height: 134px;width: 235px;margin-top: -21px;"></iframe>
+                        <h5
+                            class="name" style="font-weight: bold;color: #f7eed1;">Céline B</h5>
+                            <p class="title" style="color: #f7eed1;margin-left: 2px;">Participante 2019</p>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
                 </div>
             </div>
         </div>
