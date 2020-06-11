f95690d919cb60bd16a223f1bb8803781b4270da
Quentincaron1999 <quentincaron1999@live.fr>
2020-06-11 13:44:12 +0200

    Text --> ivoire

--- THP_académie_new/Actualite.php
+++ THP_académie_new/Actualite.php
@@ -7,19 +7,19 @@
 <body>
 
     <?php include("components/navbar.php")?>
-    <div class="columns principal is-mobile is-multiline has-text-centered mt-6 is-flex">
-        <div class="column is-half-fullhd is-full-touch" style="background-color: #173542">
-            <h1 class="title">Article</h1>
+    <div class="columns principal is-mobile is-multiline has-text-centered mt-6 is-flex bg-col">
+        <div class="column is-half-fullhd is-full-touch">
+            <h1 class="mt-6 ivoire">Article</h1>
         </div>
-        <div class="column is-full-touch" style="background-color: #09AB42">
-            <h1 class="title">Facebook</h1>
+        <div class="column is-full-touch">
+            <h1 class=" mt-6 ivoire">Facebook</h1>
             <div style="margin-top: 20px;"><iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fthpacademie%2Fposts%2F1066969263699035&show_text=true&width=552&height=692&appId" width="80%" height="" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe></div>
                     <div style="margin-top: 20px;"><iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fthpacademie%2Fposts%2F1050757068653588&show_text=true&width=552&height=515&appId" width="80%" height="" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media" ></iframe></div>
                    <div style="margin-top: 20px;"><iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fthpacademie%2Fposts%2F1002882230107739&show_text=true&width=552&height=534&appId" width="80%" height="" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe></div>
                 
         </div>
-        <div class="column is-full-touch" style="background-color: #B21B42">
-            <h1 class="title">Temoignages</h1>
+        <div class="column is-full-touch">
+            <h1 class="mt-6 ivoire">Temoignages</h1>
         </div>
     </div>
 
