f2f16f469899164cbbae338cc97e3c3364ad40be
Quentincaron1999 <quentincaron1999@live.fr>
2020-06-15 16:16:17 +0200

    temoignage-fixed

--- THP_académie_new/Actualite.php
+++ THP_académie_new/Actualite.php
@@ -13,14 +13,34 @@
         </div>
         <div class="column is-full-touch">
             <h1 class=" mt-6 ivoire">Facebook</h1>
-            <div style="margin-top: 20px;"><iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fthpacademie%2Fposts%2F1074851512910810&show_text=true&width=552&height=466&appId" width="80%" height="" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe></div>
-            <div style="margin-top: 20px;"><iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fthpacademie%2Fphotos%2Fa.903195986743031%2F1073225189740109%2F&show_text=true&width=552&height=419&appId" width="80%" height="" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe></div>
-            <div style="margin-top: 20px;"><iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fthpacademie%2Fposts%2F1066969263699035&show_text=true&width=552&height=692&appId" width="80%" height="" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe></div>
-            <div style="margin-top: 20px;"><iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fthpacademie%2Fposts%2F1050757068653588&show_text=true&width=552&height=515&appId" width="80%" height="" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media" ></iframe></div>
-            <div style="margin-top: 20px;"><iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fthpacademie%2Fposts%2F1002882230107739&show_text=true&width=552&height=534&appId" width="80%" height="" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe></div>
+            <div class="mt-6"><iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fthpacademie%2Fposts%2F1074851512910810&show_text=true&width=552&height=466&appId" width="80%" height="" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe></div>
+            <div class="mt-6"><iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fthpacademie%2Fphotos%2Fa.903195986743031%2F1073225189740109%2F&show_text=true&width=552&height=419&appId" width="80%" height="" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe></div>
+            <div class="mt-6"><iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fthpacademie%2Fposts%2F1066969263699035&show_text=true&width=552&height=692&appId" width="80%" height="" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe></div>
+            <div class="mt-6"><iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fthpacademie%2Fposts%2F1050757068653588&show_text=true&width=552&height=515&appId" width="80%" height="" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media" ></iframe></div>
+            <div class="mt-6"><iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fthpacademie%2Fposts%2F1002882230107739&show_text=true&width=552&height=534&appId" width="80%" height="" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe></div>
         </div>
         <div class="column is-full-touch">
             <h1 class="mt-6 ivoire">Temoignages</h1>
+            <div class="mt-6">
+                <iframe allowfullscreen="" frameborder="0" src="https://www.youtube-nocookie.com/embed/RiGs0YiPDtg?autoplay=1&amp;mute=1&amp;loop=1&amp;playlist=RiGs0YiPDtg&amp;controls=0" style="height: 134px;width: 235px;"></iframe>
+                <h5 class="tem-title">Vincent G</h5>
+                <p class="tem-name">Participant 2019</p>
+            </div>
+            <div class="mt-6">
+                <iframe allowfullscreen="" frameborder="0" src="https://www.youtube-nocookie.com/embed/pSN_53lo-uo?autoplay=1&amp;mute=1&amp;loop=1&amp;playlist=pSN_53lo-uo&amp;controls=0" style="height: 134px;width: 235px;"></iframe>
+                <h5 class="tem-title" style="margin-left: -10rem;">Cathy A</h5>
+                <p class="tem-name">Participante 2019</p>
+            </div>
+            <div class="mt-6">
+                <iframe allowfullscreen="" frameborder="0" src="https://www.youtube-nocookie.com/embed/5S0_MXcVGPg?autoplay=1&amp;mute=1&amp;loop=1&amp;playlist=5S0_MXcVGPg&amp;controls=0" style="height: 134px;width: 235px;"></iframe>
+                <h5 class="tem-title">Jérôme D</h5>
+                <p class="tem-name">Participant 2019</p>
+            </div>
+            <div class="mt-6">
+                <iframe allowfullscreen="" frameborder="0" src="https://www.youtube-nocookie.com/embed/kT6I_R0J_GU?autoplay=1&amp;mute=1&amp;loop=1&amp;playlist=kT6I_R0J_GU&amp;controls=0" style="height: 134px;width: 235px;"></iframe>
+                <h5 class="tem-title" style="margin-left: -10rem;">Céline B</h5>
+                <p class="tem-name">Participante 2019</p>
+            </div>
         </div>
     </div>
 
