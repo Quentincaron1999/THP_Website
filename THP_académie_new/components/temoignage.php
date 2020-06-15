8f153fa079c2b4e82b5c1cdb04e520a7b8c1731a
Quentincaron1999 <quentincaron1999@live.fr>
2020-06-15 10:47:06 +0200

    clean code

--- THP_académie_new/components/temoignage.php
+++ THP_académie_new/components/temoignage.php
@@ -15,23 +15,25 @@
                 </div>
             </div>
             <div class="column">
-            <div class="author"><iframe allowfullscreen="" frameborder="0" src="https://www.youtube-nocookie.com/embed/pSN_53lo-uo?autoplay=1&amp;mute=1&amp;loop=1&amp;playlist=pSN_53lo-uo&amp;controls=0" style="height: 134px;width: 235px;margin-top: -21px;"></iframe>
-                        <h5 class="name" style="font-weight: bold;color: #f7eed1;">Cathy A</h5>
+                <div class="author">
+                    <iframe allowfullscreen="" frameborder="0" src="https://www.youtube-nocookie.com/embed/pSN_53lo-uo?autoplay=1&amp;mute=1&amp;loop=1&amp;playlist=pSN_53lo-uo&amp;controls=0" style="height: 134px;width: 235px;margin-top: -21px;"></iframe>
+                    <h5 class="name" style="font-weight: bold;color: #f7eed1;">Cathy A</h5>
                     <p class="title" style="color: #f7eed1;margin-left: 1px;">Participante 2019</p>
-                    </div>
+                </div>
             </div>
             <div class="column">
-            <div class="author"><iframe allowfullscreen="" frameborder="0" src="https://www.youtube-nocookie.com/embed/5S0_MXcVGPg?autoplay=1&amp;mute=1&amp;loop=1&amp;playlist=5S0_MXcVGPg&amp;controls=0" style="height: 134px;width: 235px;margin-top: -21px;"></iframe>
-                        <h5
-                            class="name" style="font-weight: bold;color: #f7eed1;">Jérôme D</h5>
-                            <p class="title" style="color: #f7eed1;margin-left: 1px;">Participant 2019</p>
-                    </div>
+                <div class="author">
+                    <iframe allowfullscreen="" frameborder="0" src="https://www.youtube-nocookie.com/embed/5S0_MXcVGPg?autoplay=1&amp;mute=1&amp;loop=1&amp;playlist=5S0_MXcVGPg&amp;controls=0" style="height: 134px;width: 235px;margin-top: -21px;"></iframe>
+                    <h5 class="name" style="font-weight: bold;color: #f7eed1;">Jérôme D</h5>
+                    <p class="title" style="color: #f7eed1;margin-left: 1px;">Participant 2019</p>
+                </div>
             </div>
             <div class="column">
-            <div class="author"><iframe allowfullscreen="" frameborder="0" src="https://www.youtube-nocookie.com/embed/kT6I_R0J_GU?autoplay=1&amp;mute=1&amp;loop=1&amp;playlist=kT6I_R0J_GU&amp;controls=0" style="height: 134px;width: 235px;margin-top: -21px;"></iframe>
-                        <h5 class="name" style="font-weight: bold;color: #f7eed1;">Céline B</h5>
-                        <p class="title" style="color: #f7eed1;margin-left: 2px;">Participante 2019</p>
-                    </div>
+                <div class="author">
+                    <iframe allowfullscreen="" frameborder="0" src="https://www.youtube-nocookie.com/embed/kT6I_R0J_GU?autoplay=1&amp;mute=1&amp;loop=1&amp;playlist=kT6I_R0J_GU&amp;controls=0" style="height: 134px;width: 235px;margin-top: -21px;"></iframe>
+                    <h5 class="name" style="font-weight: bold;color: #f7eed1;">Céline B</h5>
+                    <p class="title" style="color: #f7eed1;margin-left: 2px;">Participante 2019</p>
+                </div>
             </div>
         </div>
         <div class="container">
