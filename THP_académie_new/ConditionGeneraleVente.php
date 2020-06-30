c2475d05b1e50a422dad18b0d84c4d7317182817
Quentincaron1999 <quentincaron1999@live.fr>
2020-06-30 15:53:05 +0200

    Orthograph modif

--- THP_académie_new/ConditionGeneraleVente.php
+++ THP_académie_new/ConditionGeneraleVente.php
@@ -42,7 +42,7 @@
                         <h1 class=" ">Article 3 : Prix</h1>
                         <p class="has-text-justified is-size-5-fullhd mt-4  ">
                             Les prix annoncés sur le site internet thpacademie.com sont indiqués en euros et toutes taxes comprises. Ils comprennent la pension complète, l’encadrement, les analyses vidéos pendant la durée de la Prestation. Ces prix ne comprennent pas l’assurance
-                            annulation proposée aumoment de l’inscription par notre prestataire de plateforme d’inscription, IKINOA. Ils ne comprennent pas non plus les déplacements pour se rendre sur le lieu de la prestation, les assurances individuelles, les compléments
+                            annulation proposée au moment de l’inscription par notre prestataire de plateforme d’inscription, IKINOA. Ils ne comprennent pas non plus les déplacements pour se rendre sur le lieu de la prestation, les assurances individuelles, les compléments
                             alimentaires (barres boissons énergétiques), les dépenses personnelles, les boissons pendant les repas, les nuitées supplémentaires ou suppléments chambre simple, les transferts individuels en cas d’interruption de la sortie ou dela Prestation.<br><br>
                         </p>
                     </div>
@@ -53,7 +53,7 @@
                     <div class="container">
                         <h1 class=" ">Article 4 : Inscription et paiement des prestations</h1>
                         <p class="has-text-justified is-size-5-fullhd mt-4  ">
-                            L’inscription à la Prestation et son paiement se font en ligne via la plateforme sécurisée de notre prestataire IKINOA. L’inscription est réputée définitive par le règlement total du prixde la Prestation.
+                            L’inscription à la Prestation et son paiement se font en ligne via la plateforme sécurisée de notre prestataire IKINOA. L’inscription est réputée définitive par le règlement total du prix de la Prestation.
                         </p>
                     </div>
                 </div>
@@ -63,11 +63,11 @@
                     <div class="container">
                         <h1 class=" ">Article 5 : Modification et annulation</h1>
                         <p class="has-text-justified is-size-5-fullhd mt-4  ">
-                            L’Association se réserve le droit de modifier et/ou d’annuler une Prestation avant le début decelle-ci, notamment si les effectifs sont insuffisants ou pour des raisons de sécurité. En cas d’annulation de toute ou partie de la Prestation indépendante
+                            L’Association se réserve le droit de modifier et/ou d’annuler une Prestation avant le début de celle-ci, notamment si les effectifs sont insuffisants ou pour des raisons de sécurité. En cas d’annulation de toute ou partie de la Prestation indépendante
                             de la volonté de l’Association (conditions météorologiques, décision des autorités administratives, etc.), aucun remboursement ne sera effectué.
                         </p>
                         <p class="has-text-justified is-size-5-fullhd mt-4  ">
-                            Modification ou annulation par le client: Notre prestataire pour la plateforme d’inscription sécurisée propose une assurance annulation disponible uniquement au moment del’inscription. Si cette assurance annulation aété contractée au moment de
+                            Modification ou annulation par le client : Notre prestataire pour la plateforme d’inscription sécurisée propose une assurance annulation disponible uniquement au moment de l’inscription. Si cette assurance annulation a été contractée au moment de
                             l’inscription, le client pourra être remboursé selon les termes proposés par cette assurance. A défaut, l’annulation de la réservation par le client ne pourra entraîner aucun remboursement du montant de la Prestation par l’Association.
                          </p>
                     </div>
@@ -79,19 +79,7 @@
                         <h1 class=" ">Article 6 : Droits à l’image</h1>
                         <p class="has-text-justified is-size-5-fullhd mt-4  ">
                             Chaque client.e autorise expressément l’Association ainsi que ses ayants droit tels que les partenaires et médias à utiliser les images fixes ou audiovisuelles sur lesquelles il pourrait apparaître, prises à l’occasion de sa participation à la
-                            prestation sur tous supports y comprisles documents promotionnels et/ou publicitaires, dans le monde entier et pour la durée la plus longue prévue par la loi, les règlements, les traités en vigueur, y compris pour les prolongations éventuelles
-                            qui pourraient être apportées à cette durée.
-                        </p>
-                    </div>
-                </div>
-            </section>
-            <section  class="hero">
-                <div class="hero-body">
-                    <div class="container">
-                        <h1 class=" ">Article 6 : Droits à l’image</h1>
-                        <p class="has-text-justified is-size-5-fullhd mt-4  ">
-                            Chaque client.e autorise expressément l’Association ainsi que ses ayants droit tels que les partenaires et médias à utiliser les images fixes ou audiovisuelles sur lesquelles il pourrait apparaître, prises à l’occasion de sa participation à la
-                            prestation sur tous supports y comprisles documents promotionnels et/ou publicitaires, dans le monde entier et pour la durée la plus longue prévue par la loi, les règlements, les traités en vigueur, y compris pour les prolongations éventuelles
+                            prestation sur tous supports y compris les documents promotionnels et/ou publicitaires, dans le monde entier et pour la durée la plus longue prévue par la loi, les règlements, les traités en vigueur, y compris pour les prolongations éventuelles
                             qui pourraient être apportées à cette durée.
                         </p>
                     </div>
@@ -102,7 +90,7 @@
                     <div class="container">
                         <h1 class=" ">Article 7 : Droits photos</h1>
                         <p class="has-text-justified is-size-5-fullhd mt-4  ">
-                            Toutes les photos et vidéos prises à titre personnel par les clients pendant la prestation ne peuvent faire l’objet d’aucune publication et/ou exploitation à des fins commerciales quelleque soit la forme de support (presse, magazine, site internet,
+                            Toutes les photos et vidéos prises à titre personnel par les clients pendant la prestation ne peuvent faire l’objet d’aucune publication et/ou exploitation à des fins commerciales quelle que soit la forme de support (presse, magazine, site internet,
                             etc.). L’Association, organisatrice de la Prestation est propriétaire de tous les droits photos et vidéos et doit donner son accord express écrit avant toute forme d’exploitation.
                         </p>
                     </div>
@@ -145,9 +133,9 @@
                         <h1 class=" ">Article 10 : Loi informatique et liberté</h1>
                         <p class="has-text-justified is-size-5-fullhd mt-4  ">
                             Conformément à la loi n°78-17 du 6 janvier 1978 relative à l’informatique, aux fichiers et aux libertés, dite loi « Informatique et Libertés », au Règlement UE (2016/679) Général sur la Protection des Données du 27 avril 2016, dit « RGPD », le
-                            client bénéficie d’un droit d’accès,de rectification, de portabilité et d’effacement de celles-ci. Il peut également opposer à l’Association, pour motifs légitimes, au traitement de ses données, et dispose du droit deretirer son consentement
-                            à tout moment. Il peut enfin s’opposer à l’utilisation de ses données à des fins de prospection commerciale. Pour exercer ses droits, le client peut sans frais et àtout moment adresser sa demande accompagnée d’un justificatif d’identité par
-                            voie postale à: Outdoor Events In Provence–Service données personnelles, 8 avenue Eugène Bernard, 04300 FORCALQUIER, ou par courrier électronique à : contact@thpacademie.com
+                            client bénéficie d’un droit d’accès, de rectification, de portabilité et d’effacement de celles-ci. Il peut également opposer à l’Association, pour motifs légitimes, au traitement de ses données, et dispose du droit de retirer son consentement
+                            à tout moment. Il peut enfin s’opposer à l’utilisation de ses données à des fins de prospection commerciale. Pour exercer ses droits, le client peut sans frais et à tout moment adresser sa demande accompagnée d’un justificatif d’identité par
+                            voie postale à : Outdoor Events In Provence – Service données personnelles, 8 avenue Eugène Bernard, 04300 FORCALQUIER, ou par courrier électronique à : contact@thpacademie.com
                         </p>
                     </div>
                 </div>
@@ -157,7 +145,7 @@
                     <div class="container">
                         <h1 class=" ">Article 11 : Propriété intellectuelle</h1>
                         <p class="has-text-justified is-size-5-fullhd mt-4  ">
-                            Tous les éléments présentés sur ce site sont protégés par le droit d’auteur et plus largementpar le droit de propriété intellectuelle. Toute utilisation, modification, reproduction, représentation et traduction, intégrale ou partielle, des éléments
+                            Tous les éléments présentés sur ce site sont protégés par le droit d’auteur et plus largement par le droit de propriété intellectuelle. Toute utilisation, modification, reproduction, représentation et traduction, intégrale ou partielle, des éléments
                             de ce site par quelque procédé que ce soit, sans le consentement écrit de l’Association donnera lieu à des poursuites.
                         </p>
                     </div>
